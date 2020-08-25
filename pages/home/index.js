cf3ab8e45a4896feb35dd91255488727cd80af13
juliasjogren <juliaadelasjogren@gmail.com>
2020-08-25 17:41:32 +0200

    implement show all on home page

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,20 +1,29 @@
-//HOME
 import styles from "./style.module.css";
 import ProductList from "components/productList";
 import Button from "@material-ui/core/Button";
-import Link from "next/link";
 import { useState } from "react";
+import { productData } from "data.js";
+
+function getTotalKo2(product) {
+  return product.framställning + product.frakt + product.tvätt + product.återvinning + product.odling;
+}
 
 const Home = () => {
-  const [input, setInput] = useState("Explore");
+  const [showAll, setShowAll] = useState(false);
+
+  const products = productData.sort((a, b) => getTotalKo2(a) - getTotalKo2(b));
+
+  const productsToShow = showAll ? products : products.slice(0, 5);
+
   return (
     <div className={styles.home}>
-      <ProductList type={"explore"} />
-      <Link href={`/productlist/${input}`}>
-        <Button className={styles.button} variant="contained">
+      <ProductList products={productsToShow} />
+
+      {!showAll && (
+        <Button onClick={() => setShowAll(true)} className={styles.button} variant="contained">
           Explore More...
         </Button>
-      </Link>
+      )}
     </div>
   );
 };
