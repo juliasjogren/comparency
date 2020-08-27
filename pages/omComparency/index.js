3175dad67903635f141f8de8d7fa4e886be076ce
Victor Friberg <friberg.victor@gmail.com>
2020-08-27 22:52:59 +0200

    Initiate new About Comparency page

--- pages/omComparency/index.js
+++ pages/omComparency/index.js
@@ -1,5 +1,45 @@
-const OmComparency = () => {
-  return <div>Om Comparency</div>;
-};
+import { Grid, Paper } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 
-export default OmComparency;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    background: '#a0adad'
+  },
+}));
+
+export default function OmComparency() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>Lite text</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>Mer text</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>Annan text</Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>Text</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>Ännu mer text</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>Text igen</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
